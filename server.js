17f9a2d912ed16708f7fb74bb6856eeca41bb4c4
Bryan Castleman <castlemaninc@gmail.com>
2017-05-26 19:04:43 -0700

    adding require sequelize to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
+var Sequelize = require("sequelize");
 
 var app = express();
 // Serve static content for the app from the "public" directory in the application directory.
