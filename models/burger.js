3ddb2a4d6d525a91e587a4fd75ebb5fcd72e8ea0
Bryan Castleman <castlemaninc@gmail.com>
2017-05-27 22:05:08 -0700

    removed commented out code from burger model

--- models/burger.js
+++ models/burger.js
@@ -1,27 +1,4 @@
 
-// Dependencies 
-// var Sequelize = require("sequelize");
-// var sequelize = require("../config/connection.js");
-
-// // define the data in the burgers_db 
-// var Burger = sequelize.define("burgers", {
-//   id: {
-//     type: Sequelize.INTEGER,
-//     autoIncrement: true,
-//     primaryKey: true
-//   },
-//   burger_name: Sequelize.STRING,
-//   devoured: Sequelize.BOOLEAN
-// }, {
-//   timestamps: false
-// });
-
-
-
-
-// module.exports = Burger;
-
-
 module.exports = function(sequelize, DataTypes) {
 	var burgers = sequelize.define("burgers", {
     
