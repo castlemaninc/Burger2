9414c40b5cc22045522efdf9a25b7f8f8f040ea5
Bryan Castleman <castlemaninc@gmail.com>
2017-05-27 20:06:28 -0700

    commented out connection.js and changed the burger model to export a function

--- models/burger.js
+++ models/burger.js
@@ -1,22 +1,44 @@
 
 // Dependencies 
-var Sequelize = require("sequelize");
-var sequelize = require("../config/connection.js");
+// var Sequelize = require("sequelize");
+// var sequelize = require("../config/connection.js");
 
-// define the data in the burgers_db 
-var Burger = sequelize.define("burgers", {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    primaryKey: true
-  },
-  burger_name: Sequelize.STRING,
-  devoured: Sequelize.BOOLEAN
-}, {
-  timestamps: false
-});
+// // define the data in the burgers_db 
+// var Burger = sequelize.define("burgers", {
+//   id: {
+//     type: Sequelize.INTEGER,
+//     autoIncrement: true,
+//     primaryKey: true
+//   },
+//   burger_name: Sequelize.STRING,
+//   devoured: Sequelize.BOOLEAN
+// }, {
+//   timestamps: false
+// });
 
 
-Burger.sync();
+// Burger.sync();
 
-module.exports = Burger;
+// module.exports = Burger;
+
+module.exports = function(sequelize, DataTypes) {
+  var Burger = sequelize.define("Burger", {
+    // Giving the Author model a name of type STRING
+    id: {
+	    type: Sequelize.INTEGER,
+	    autoIncrement: true,
+	    primaryKey: true
+	},
+		burger_name: Sequelize.STRING,
+		devoured: Sequelize.BOOLEAN
+	}, {
+		timestamps: false
+	}, {
+	    classMethods: {
+	      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return burger;
+};
