5b39ff1d59cd88a3eb73171fe91a2cff34f0be8e
Bryan Castleman <castlemaninc@gmail.com>
2017-05-28 10:08:34 -0700

    modifying burger.js model

--- models/burger.js
+++ models/burger.js
@@ -2,21 +2,21 @@
 module.exports = function(sequelize, DataTypes) {
 	var burgers = sequelize.define("burgers", {
     
-	    id: {
-		    type: DataTypes.INTEGER,
-		    autoIncrement: true,
-		    primaryKey: true
-		}, 
+	 //    id: {
+		//     type: DataTypes.INTEGER,
+		//     autoIncrement: true,
+		//     primaryKey: true
+		// }, 
 		burger_name: DataTypes.STRING,
 		devoured: DataTypes.BOOLEAN
 	}, {
 		timestamps: false
-	}, {
-	    classMethods: {
-			associate: function(models) {
-	        // associations can be defined here
-			}
-	    }
+	// }, {
+	//     classMethods: {
+	// 		associate: function(models) {
+	//         // associations can be defined here
+	// 		}
+	    // }
   });
 
   return burgers;
